1a6518ca489e6fb88c2df371e8a6badda37408c5
Matheus Menezes Scaranello <123407141+MatheusScaranello@users.noreply.github.com>
2024-06-14 22:31:53 -0300

    Update index.jsx

--- src/screens/Introducao/index.jsx
+++ src/screens/Introducao/index.jsx
@@ -4,6 +4,7 @@ import { Ionicons } from "@expo/vector-icons";
 import { Audio } from 'expo-av';
 import * as Speech from 'expo-speech';
 import { useNavigation } from "@react-navigation/native";
+import styles from "./styles";
 
 const Introducao = () => {
   const navigation = useNavigation();
@@ -73,10 +74,44 @@ const Introducao = () => {
         textToRead = 'Bem-vindo ao Mundo da Inteligência Artificial! Descubra como a IA está transformando o nosso dia a dia.';
         break;
       case 'Beneficios':
-        textToRead = 'Benefícios da IA. A IA pode melhorar a eficiência, reduzir custos, aumentar a precisão e personalizar experiências.';
+        textToRead = `Benefícios da IA. A IA pode melhorar a eficiência, reduzir custos, aumentar a precisão e personalizar experiências.
+        
+        Maior eficácia na tomada de decisões e na execução de tarefas, com maior precisão;
+        
+        Tem a capacidade de funcionar de maneira ininterrupta e realizar tarefas repetitivas;
+        
+        Resultou em melhorias no processo de compra e venda online.`;
         break;
       case 'Desvantagens':
-        textToRead = 'Desvantagens da IA. A IA pode levar à perda de empregos, criar vieses algorítmicos, e levantar questões éticas.';
+        textToRead = `Desvantagens da IA. A IA pode levar à perda de empregos, criar vieses algorítmicos, e levantar questões éticas.
+        
+        Apresentam elevado custo de desenvolvimento e implementação, sendo um recurso restrito;
+        
+        A automação de tarefas e da tomada de decisões tendem a ocasionar desemprego estrutural;
+        
+        Têm avanços limitados mesmo com maior acúmulo de experiência.`;
+        break;
+      case 'ComoFunciona':
+        textToRead = `Como funciona a inteligência artificial. O Funcionamento da inteligência artificial, de maneira simplificada, acontece por meio da coleta e de combinação de um grande volume de dados seguido da identificação de determinados padrões nesse conjunto de informações.Com esse processo, que geralmente se dá mediante a utilização de algoritmos pré-programados, o software consegue tomar decisões e realizar tarefas de maneira autônoma.
+
+        Métodos da IA:
+        
+        Machine learning:
+        
+        chamado de aprendizado de máquina, é o processo que acontece de maneira automatizada. O reconhecimento e a reprodução de padrões feitos pela IA com base na sua experiência prévia, adiquiridos pela utilização de algoritmos.
+        
+        Deep learning:
+        
+        subcampo do machine learning, utiliza-se de redes neurais (unidades conectadas em rede para análise de bancos de dados e informações) para emular o cérebro humano.`;
+        break;
+      case 'Exemplos':
+        textToRead = `Exemplos de inteligência artificial no dia a dia:
+        
+        Assistentes de voz;
+        
+        Reconhecimento facial;
+        
+        Redes sociais.`;
         break;
       default:
         break;
@@ -100,7 +135,15 @@ const Introducao = () => {
         return (
           <View style={styles.content}>
             <Text style={styles.title}>Benefícios da IA</Text>
-            <Text style={styles.text}>A IA pode melhorar a eficiência, reduzir custos, aumentar a precisão e personalizar experiências.</Text>
+            <Text style={styles.text}>
+              A IA pode melhorar a eficiência, reduzir custos, aumentar a precisão e personalizar experiências.
+              
+              Maior eficácia na tomada de decisões e na execução de tarefas, com maior precisão;
+              
+              Tem a capacidade de funcionar de maneira ininterrupta e realizar tarefas repetitivas;
+              
+              Resultou em melhorias no processo de compra e venda online.
+            </Text>
             <TouchableOpacity style={styles.button} onPress={() => handlePress('Desvantagens')}>
               <Text style={styles.buttonText}>Ver Desvantagens</Text>
             </TouchableOpacity>
@@ -115,7 +158,53 @@ const Introducao = () => {
         return (
           <View style={styles.content}>
             <Text style={styles.title}>Desvantagens da IA</Text>
-            <Text style={styles.text}>A IA pode levar à perda de empregos, criar vieses algorítmicos, e levantar questões éticas.</Text>
+            <Text style={styles.text}>
+              A IA pode levar à perda de empregos, criar vieses algorítmicos, e levantar questões éticas.
+              
+              Apresentam elevado custo de desenvolvimento e implementação, sendo um recurso restrito;
+              
+              A automação de tarefas e da tomada de decisões tendem a ocasionar desemprego estrutural;
+              
+              Têm avanços limitados mesmo com maior acúmulo de experiência.
+            </Text>
+            <TouchableOpacity style={styles.button} onPress={() => handlePress('ComoFunciona')}>
+              <Text style={styles.buttonText}>Como Funciona</Text>
+            </TouchableOpacity>
+          </View>
+        );
+      case 'ComoFunciona':
+        return (
+          <View style={styles.content}>
+            <Text style={styles.title}>Como Funciona a IA</Text>
+            <Text style={styles.text}>
+              O Funcionamento da inteligência artificial, de maneira simplificada, acontece por meio da coleta e de combinação de um grande volume de dados seguido da identificação de determinados padrões nesse conjunto de informações.Com esse processo, que geralmente se dá mediante a utilização de algoritmos pré-programados, o software consegue tomar decisões e realizar tarefas de maneira autônoma.
+              
+              Métodos da IA:
+              
+              Machine learning:
+              
+              chamado de aprendizado de máquina, é o processo que acontece de maneira automatizada. O reconhecimento e a reprodução de padrões feitos pela IA com base na sua experiência prévia, adiquiridos pela utilização de algoritmos.
+              
+              Deep learning:
+              
+              subcampo do machine learning, utiliza-se de redes neurais (unidades conectadas em rede para análise de bancos de dados e informações) para emular o cérebro humano.
+            </Text>
+            <TouchableOpacity style={styles.button} onPress={() => handlePress('Exemplos')}>
+              <Text style={styles.buttonText}>Exemplos de IA</Text>
+            </TouchableOpacity>
+          </View>
+        );
+      case 'Exemplos':
+        return (
+          <View style={styles.content}>
+            <Text style={styles.title}>Exemplos de IA no Dia a Dia</Text>
+            <Text style={styles.text}>
+              Assistentes de voz;
+              
+              Reconhecimento facial;
+              
+              Redes sociais.
+            </Text>
             <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Game')}>
               <Text style={styles.buttonText}>Jogar</Text>
             </TouchableOpacity>
@@ -133,7 +222,7 @@ const Introducao = () => {
           {renderContent()}
         </Animated.View>
         <View style={styles.progressBar}>
-          <View style={[styles.progressIndicator, { width: `${(progress / 3) * 100}%` }]} />
+          <View style={[styles.progressIndicator, { width: `${(progress / 5) * 100}%` }]} />
         </View>
         <TouchableOpacity style={styles.audioButton} onPress={() => setAudioStatus(!audioStatus)}>
           <Ionicons name={audioStatus ? 'volume-high' : 'volume-mute'} size={24} color="black" />
@@ -141,70 +230,6 @@ const Introducao = () => {
       </ScrollView>
     </ImageBackground>
   );
-};
-
-const styles = {
-  container: {
-    flexGrow: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  content: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    paddingHorizontal: 20,
-    minWidth: '100%', // Adicionado para garantir que o conteúdo não ultrapasse a largura da tela
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 10,
-    textAlign: "center",
-  },
-  subtitle: {
-    fontSize: 18,
-    marginBottom: 20,
-    textAlign: "center",
-  },
-  text: {
-    fontSize: 16,
-    textAlign: "center",
-    marginBottom: 20,
-  },
-  button: {
-    backgroundColor: "#007bff",
-    padding: 10,
-    borderRadius: 5,
-    marginTop: 20,
-    marginBottom: 10,
-  },
-  buttonText: {
-    color: "#fff",
-    fontSize: 18,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  progressBar: {
-    position: "absolute",
-    bottom: 20,
-    left: 20,
-    right: 20,
-    height: 10,
-    backgroundColor: "rgba(255, 255, 255, 0.5)",
-    borderRadius: 5,
-  },
-  progressIndicator: {
-    height: 10,
-    backgroundColor: "#007bff",
-    borderRadius: 5,
-  },
-  audioButton: {
-    position: "absolute",
-    bottom: 20,
-    right: 20,
-    zIndex: 1,
-  },
 };
 
 export default Introducao;
