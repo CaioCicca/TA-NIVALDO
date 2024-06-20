73a76f988db1f46902c0a95c4aa307f074af3024
CaioCicca <caio.ciccarelli@aluno.senai.br>
2024-06-20 08:00:19 -0300

    aaa

--- src/screens/Game/index.jsx
+++ src/screens/Game/index.jsx
@@ -14,16 +14,28 @@ const Game = () => {
   const [highScores, setHighScores] = useState([]);
   const [health, setHealth] = useState(100);
   const [info, setInfo] = useState([
-    { text: "Vacinas são seguras e eficazes.", correct: true },
-    { text: "Exercícios regulares são importantes para a saúde.", correct: true },
-    { text: "Mantenha uma dieta equilibrada para se manter saudável.", correct: true },
-    { text: "Notícias falsas podem causar danos à saúde.", correct: true },
-    { text: "Consulte um médico antes de confiar em informações de saúde.", correct: true },
-    { text: "Beber água regularmente é essencial para o corpo humano.", correct: true },
-    { text: "Dormir o suficiente é crucial para o bem-estar.", correct: true },
-    { text: "Cuidado com remédios caseiros não comprovados cientificamente.", correct: true },
-    { text: "O sol gira ao redor da Terra.", correct: false },
-    { text: "Beber café desidrata.", correct: false },
+    { text: "A IA pode melhorar a eficiência, reduzir custos, aumentar a precisão e personalizar experiências.", correct: true },
+    { text: "A IA pode levar à perda de empregos devido à automação de tarefas e tomada de decisões.", correct: false },
+    { text: "A IA possui a capacidade de funcionar de maneira ininterrupta e realizar tarefas repetitivas.", correct: true },
+    { text: "A automação de tarefas e da tomada de decisões pela IA tende a ocasionar desemprego estrutural.", correct: false },
+    { text: "A IA pode resultar em melhorias no processo de compra e venda online.", correct: true },
+    { text: "A IA tem avanços ilimitados mesmo com maior acúmulo de experiência.", correct: false },
+    { text: "A IA apresenta um elevado custo de desenvolvimento e implementação, sendo um recurso restrito.", correct: true },
+    { text: "A IA pode criar vieses algorítmicos devido à maneira como os dados são coletados e analisados.", correct: true },
+    { text: "A IA pode levantar questões éticas devido ao seu uso e impacto na sociedade.", correct: true },
+    { text: "O deep learning utiliza redes neurais para emular o cérebro humano.", correct: false },
+    { text: "A IA tem sido criticada por seu potencial de substituir empregos tradicionais por automação.", correct: true },
+    { text: "A IA pode ser utilizada para personalizar recomendações de produtos com base no histórico de compras de um usuário.", correct: true },
+    { text: "O machine learning é um processo automatizado de reconhecimento e reprodução de padrões pela IA.", correct: true },
+    { text: "A IA utiliza algoritmos pré-programados para tomar decisões e realizar tarefas de maneira autônoma.", correct: true },
+    { text: "A IA pode melhorar a segurança cibernética ao identificar padrões de comportamento suspeito na rede.", correct: true },
+    { text: "A IA pode ser implementada em sistemas de saúde para análise de grandes volumes de dados médicos.", correct: true },
+    { text: "A inteligência artificial funciona através da coleta e combinação de um grande volume de dados para identificar padrões.", correct: true },
+    { text: "O reconhecimento facial é uma aplicação de IA que se utiliza de algoritmos para identificar rostos.", correct: true },
+    { text: "Redes sociais são exemplos de plataformas que utilizam inteligência artificial para análise de dados e personalização de conteúdo.", correct: true },
+    { text: "A IA pode contribuir para a previsão de tendências de mercado com base em dados históricos e comportamentais.", correct: true },
+    { text: "A IA pode criar vieses éticos devido à maneira como os algoritmos são programados.", correct: false }
+
   ]);
   const [gameOver, setGameOver] = useState(false);
   const [victory, setVictory] = useState(false);
